0250fe0c60ad8191a8aae3ff4267d1ed64189fe8
Firdous Seid <itsfiri@gmail.com>
2024-08-24 01:26:35 -0700

    to do list commit.

---
+++ addTask.php
@@ -0,0 +1,15 @@
+
+<header>
+        <h1>Add New Task</h1>
+
+    </header>
+    <form method="post">
+        <label for="taskname">Task Name</label>
+        <input type="text" id="taskname" required>
+        <label for="description">Description</label>
+        <input type="text" id="description"  required>
+        <label for="duedate">Due Date</label>
+        <input type="date"  id="duedate"  required>
+        <button type="submit">Add Task</button>
+    </form>
+    ?>
